90ac681918e4a6580268aeb929a1b029854f98be
PaperTTowel <mycat210117@icloud.com>
2025-06-15 11:08:21 +0900

    Changes: 윈도우 환경에서 cmake 경로 로컬 지정           - set PATH=C:\msys64\ucrt64\bin;C:\msys64\usr\bin;%PATH%

--- .env.cmake
+++ .env.cmake
@@ -1,7 +1,7 @@
 set(GLFW_PATH C:/msys64/ucrt64)
 set(BULLET_SDK_PATH C:/msys64/ucrt64)
 set(GLM_PATH C:/msys64/ucrt64/include)
-set(VULKAN_SDK_PATH C:/VulkanSDK/1.4.304.1)
+set(VULKAN_SDK_PATH C:/VulkanSDK/1.4.313.1)
  
 # Set MINGW_PATH if using mingwBuild.bat and not VisualStudio20XX
 set(MINGW_PATH "C:/msys64/mingw-w64/x86_64-8.1.0-posix-seh-rt_v6-rev0/mingw64")
--- msys2Build.bat
+++ msys2Build.bat
@@ -1,6 +1,9 @@
 @echo off
 
-if not exist build  mkdir build
+REM MSYS2 PATH 덮어쓰기
+set PATH=C:\msys64\ucrt64\bin;C:\msys64\usr\bin;%PATH%
+
+if not exist build mkdir build
 cd build
 cmake -S ../ -B . -G "MSYS Makefiles" -DCMAKE_MAKE_PROGRAM=C:/msys64/ucrt64
 C:/msys64/usr/bin/make.exe && C:/msys64/usr/bin/make.exe Shaders
